3314708a7e0b9ee436c78fb3475a335333320bb6
sairam <sairm21@gmail.com>
2023-10-09 22:12:52 +0530

    adding route53 records for rabbitmq module

--- userdata.sh
+++ userdata.sh
@@ -2,6 +2,6 @@
 
 labauto ansible
 
-set-hostname -skip-apply ${var.component}
+set-hostname -skip-apply ${component}
 
 ansible-pull -i localhost, -U https://github.com/sairm21/roboshop-ansible-v1 -e env=${env} -e role_name=${component} main.yml &>> /opt/ansible.log
